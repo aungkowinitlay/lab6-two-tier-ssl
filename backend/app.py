2e60356a41aca435b7a9c1c4ac96396151d15cdd
aungkowinitlay <aungkowin.osit@gmail.com>
2025-04-18 20:45:15 +0630

    first commit

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=["GET"])
+@app.route("/api", methods=["GET"])
 def get_message():
     return jsonify({"message": "Hello from the backend!"})
 
